0b2e97777e328d034be45cffde768b10edd20044
Omer <omerweiss@yahoo.com>
2022-09-29 22:58:36 -0700

    made last attempts at creating solution, adjusted pseudo code and added sceenshots to readme.

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Random-Code-Generator
+
+Description
+
+My goal in working on this Random-Code-Generator was to enable users to be able to generate safe passwords using criteria of their choice. The javscript code prompts users to make choices about which criteria they want in their password. The choices are then used to figure out which set of characters will be randomly parsed through for password character values. The screen then displays the password to the user. This process involved learning and implementing javascript functions, creating global variables, if else statement and different methods, inluding math.random.
+
+Live Demo
+
+
+
+Usage
+
+The following images features a prompt box asking user to input a criteria for their password. 
+
+![ image of first prompt box](/prompt.png "Prompt")
+
+
+
+
--- script.js
+++ script.js
@@ -1,4 +1,29 @@
+// 1)
 
+// Create Building Blocks:
+
+// Prompt user length of password, inform them that their choice has to be in between 8-128, and keep prompting them if they choose cancel or the number does not meet the required criteria. Once a valid choice is made store it in a global variable.
+
+
+// Prompt if they want lower case chars “enter “yes” and press okay for yes and cancel for no” if answer is cancel (null) , or no move on to the next prompt , if answer is  yes (sanitize) and make answer then move on to next prompt
+
+// Prompt if they want upper case chars “enter “yes” and press okay for yes and cancel for no” if answer is cancel (null) move on to the next prompt , if answer is  yes (sanitize) and store all upper case chars in a global array variable.
+
+// Prompt if they want special chars “enter “yes” and press okay for yes and cancel for no” if answer is cancel (null) move on to the next prompt , if answer is  yes (sanitize) and store all special chars in a global array variable.
+
+// Prompt if they want numeric chars “enter “yes” and press okay for yes and cancel for no” if answer is cancel (null) move on to the next prompt , if answer is  yes (sanitize) and store all numeric chars in a global array variable.
+
+// 2)
+
+// Create one index array - concatenates the global arrays and stores the values in a global one index array
+
+// 3)
+
+// Create a function that generates password.
+// Create empty string variable that will store the password. Use math dot random to parse through the combined global index array and chooses a random index.Do this as many times as the value of the global length variable each time adding the random value to the empty string variable. Return the now filled up empty string variable.
+
+//Global Variables
+//Blank array for results of users choices 
 var arrayForGenerator = [];
 const lowerCaseLetters = ['a', 'b','c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's','t', 'u', 'v', 'w', 'x', 'y', 'z'];
 const upperCaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
@@ -6,11 +31,10 @@ const numericChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 const specialChars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '-', '.', '~', '|', '<', '>', '=', '-', '_', '/', ':', ';', '?', '[', ']', '{', '}', '~'];
 
 
-//prompt user for password length
 
 
 
-
+// function promts user for criteria of the password, validates respones and adds return values to global variables
 
 function getCriteria () {
     do {
@@ -26,13 +50,14 @@ function getCriteria () {
           return userInputLength;
         } else return false;
       }
-      
+    //GLobal Variable for length that user wants password
       var lengthOfPassword = userInputLength;
-      
+    // prompting user for choices of which characters go into blank array that will be parsed later, and set values to variables.  
     var lowerCaseChoice = confirm("Would you like lowercase characters in your password, click okay for yes, click cancel for no");
     var upperCaseChoice = confirm("Would you like uppercase characters in your password, click okay for yes, click cancel for no");
     var specialCharsChoice = confirm("Would you like special characters in your password, click okay for yes, click cancel for no");
     var numericCharsChoice = confirm("Would you like numeric characters in your password, click okay for yes, click cancel for no");
+    // this code takes the users choie to decide what global arrays to be added to the blank global array.
     if (lowerCaseChoice ){
         arrayForGenerator = [...arrayForGenerator,...lowerCaseLetters];
     }
@@ -46,72 +71,21 @@ function getCriteria () {
         arrayForGenerator = [...arrayForGenerator,...numericChars];
     }
     
-    return lengthOfPassword;
-    
-    // if (upperCaseChoice ){
-    //     uppersSelection += upperCaseLetters;
-    //     arrayForGenerator += uppersSelection;
-    // } 
-    // if (specialSelection ){
-    //     specialSelection += specialChars;
-    //     arrayForGenerator += specialSelection;
-    // }
-    // if (numericSelection ){
-    //     numericSelection += numericChars;
-    //     arrayForGenerator += numericSelection;
-    // }
+     
 }
 
+//this function generates a password by randomly going through our now filled blank array and choosing a random index, this process is repeated as many times as the users choice in length is, each time adding the chosen index to a empty variable "pass". When complete the now filled in variable string is returned to user. 
 function generatePassword(){
     var lengthOfPassword = getCriteria();
-    var pass = " ";
+    var password = " ";
     for (var i=0; i<= lengthOfPassword; i++ ) {
         var char = Math.floor(Math.random()
                             * arrayForGenerator.length + 1);
                   
                 pass += arrayForGenerator[char]
     }
-    return pass;
+    return password;
 }
-
-console.log(pass);
-
-
-
-
-//
-
-// if (confirm("Would you like lowercase characters in your password, click okay for yes, click cancel for no") == true){
-//     lowersSelection = true;
-//   }
-
-
-
-// //
-
-// if (confirm("Would you like upper case characters in your password, click okay for yes, click cancel for no") == true){
-//     uppersSelection = true;
-//   }
-
-
-
-// //
-
-// if (confirm("Would you like special characters in your password, click okay for yes, click cancel for no") == true){
-//     specialSelection = true;
-//   }
-
-
-
-// //
-
-// if (confirm("Would you like numeric characters in your password, click okay for yes, click cancel for no") == true) {
-//     numericSelection = true;
-// }
-
-
-
-//
 
 
 
